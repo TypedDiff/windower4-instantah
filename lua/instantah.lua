44fc7070d18ee93abc490607e2da0d35bbda9ae4
Robert Beheiter <rbeheiter@gmail.com>
2026-09-17 17:49:43 -0400

    Fixed lua

--- lua/instantah.lua
+++ lua/instantah.lua
@@ -5,15 +5,11 @@ _addon.commands = { }
 
 local addon_path = windower.addon_path:gsub('\\', '/')	
 package.cpath = package.cpath .. ';' .. addon_path .. '/libs/?.dll'
-require('MemoryLib')
+require('memorylib')
 
 local instantah = { };
 instantah.pattern = '668BC1B9????????0FAFC233D2F7F1'
-instantah.pointer = MemoryLib.findPattern('FFXiMain.dll', instantah.pattern);
-
-local nolock = { };
-nolock.pattern = '66FF81????????66C781????????0807C3'
-nolock.pointer = MemoryLib.findPattern('FFXiMain.dll', nolock.pattern)
+instantah.pointer = memorylib.findPattern('FFXiMain.dll', instantah.pattern);
 
 local function msg(s)
     local txt = '[InstantAH] ' .. s;
@@ -26,24 +22,12 @@ windower.register_event('load', function()
         return;
     end
 
-    MemoryLib.write_uint8(instantah.pointer + 0x27, 0xEB);
+    memorylib.write_uint8(instantah.pointer + 0x27, 0xEB);
     msg('Function patched; auction results should now be instant.');
-
-    if (nolock.pointer == 0) then
-        msg('Failed to find required pointer.');
-        return;
-    end
-
-    for i = 0,6
-    do
-        MemoryLib.write_uint8(nolock.pointer + i, 0x90);
-    end
-
-    msg('NoLock function patched; should no longer be locked during disengage');
 end)
 
 windower.register_event('unload', function()
     if (instantah.pointer ~= 0) then
-        MemoryLib.write_uint8(instantah.pointer + 0x27, 0x74);
+        memorylib.write_uint8(instantah.pointer + 0x27, 0x74);
     end
 end)
